2449f94ebc5da07e72c5521ad7091fd633be2781
RomanIsko <iskoroman@gmail.com>
2014-10-08 12:21:26 +0300

    add shell script to perform check if is there a newer version than 2.10 available

---
+++ chrome_driver_checker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get_display [USER] — Returns $DISPLAY of USER.
+# If first param is omitted, then $LOGNAME will be used.
+get_display () {
+who \
+| grep ${1:-$LOGNAME} \
+| perl -ne 'if ( m!\(\:(\d+)\)$! ) {print ":$1.0\n"; $ok = 1; last} END {exit !$ok}'
+}
+
+DISPLAY=$(get_display) || exit
+export DISPLAY
+
+# echo "Let's check the latest available Chrome driver version."
+last_version=$(curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+# echo "The latest available Chrome driver version is" $last_version
+if [ "$last_version" != "2.10" ]; then
+	$(notify-send "Chrome driver $last_version" "Chrome Driver version $last_version is available")
+fi
