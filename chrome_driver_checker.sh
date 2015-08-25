2808272ce7026422227e9b2a155dd4aa9b0cbbdb
RomanIsko <iskoroman@gmail.com>
2015-08-25 10:31:20 +0300

    Update script to check Chrome version higher than 2.18

--- chrome_driver_checker.sh
+++ chrome_driver_checker.sh
@@ -36,6 +36,6 @@ notify() {
 # echo "Let's check the latest available Chrome driver version."
 last_version=$(curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 # echo "The latest available Chrome driver version is" $last_version
-if [ "$last_version" != "2.16" ]; then
+if [ "$last_version" != "2.18" ]; then
     $(notify "Chrome driver $last_version" "Chrome Driver version $last_version is available" 5000)
 fi
